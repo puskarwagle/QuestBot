3d28918998f4e4e93c017a8b9be2cff2dd53540c
puskarwagl <puskarwagle17@gmail.com>
2025-07-07 02:18:35 +0530

    minor fixes

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
 
-Route::view('/', 'welcome');
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
+    Volt::route('settings/password', 'settings.password')->name('settings.password');
+    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+});
     
 Route::middleware(['auth'])->group(function () {
    Route::get('/personal-info', App\Livewire\PersonalInfoCunt::class)->name('personal-info');
